c0ea68e09e62c7ddb26773bab8a26bc8ad6eb9c4
Andrew Odeke <andrewimalingat@gmail.com>
2026-03-10 20:28:17 +0200

    Added new comments to cars.py

--- cars.py
+++ cars.py
@@ -1,3 +1,11 @@
-car_brands_list =['Toyota','Suzuki','Volkswagen']
-for index,car in enumerate(car_brands_list,start=1):
-    print(f"{car} in position: {index} in sells chart")
+# Top 3 Manufacturers (February 2026)
+february_2026_sales = {
+    "Toyota": 12272,
+    "Suzuki Auto": 6562,
+    "Volkswagen Group SA": 4895
+}
+
+# Loop through the car brand dictionary
+for key, (brand, sales) in enumerate(february_2026_sales.items(), start=1):
+    # Display naamsa top three selling car brands for the month of February 2026.
+    print(f"{key}. {brand}: {sales} units")
